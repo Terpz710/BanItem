e202129ba8e1f5742c53339dc93f6403e0e637db
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-11-19 17:59:49 -0800

    PM5-BETA

--- src/LDX/BanItem/Main.php
+++ src/LDX/BanItem/Main.php
@@ -1,144 +1,221 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LDX\BanItem;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\Player;
-use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\command\CommandSender;
 use pocketmine\event\block\BlockPlaceEvent;
-use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\event\player\PlayerItemConsumeEvent;
 use pocketmine\event\entity\EntityShootBowEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
+use pocketmine\item\Item;
+use pocketmine\item\LegacyStringToItemParser;
+use pocketmine\item\StringToItemParser;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Filesystem;
+use pocketmine\utils\TextFormat as TEXTFORMAT;
 
-class Main extends PluginBase implements Listener {
+final class Main extends PluginBase implements Listener {
 
-  public function onEnable() {
-    $this->saveItems();
-    $this->getServer()->getPluginManager()->registerEvents($this,$this);
-  }
+    /** @var string[]|null $items */
+    private ?array $items;
+    /** @var string[] $spys */
+    private array $spys = [];
 
-  public function onTouch(PlayerInteractEvent $event) {
-    $p = $event->getPlayer();
-    if($this->isBanned($event->getItem())) {
-      if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.bypass"))) {
-        $p->sendMessage("[BanItem] That item is banned.");
-        $event->setCancelled();
-      }
+    public function onEnable(): void {
+        $this->saveItems();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-  }
 
-  public function onBlockPlace(BlockPlaceEvent $event) {
-    $p = $event->getPlayer();
-    if($this->isBanned($event->getItem())) {
-      if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.bypass"))) {
-        $p->sendMessage("[BanItem] That item is banned.");
-        $event->setCancelled();
-      }
+    public function getPlayerPosition(Player $sender): string {
+        $pos = $sender->getPosition();
+        $playerX = $pos->getX();
+        $playerY = $pos->getY();
+        $playerZ = $pos->getZ();
+        $outX = round($playerX, 1);
+        $outY = round($playerY, 1);
+        $outZ = round($playerZ, 1);
+        $worldName = $sender->getWorld()->getDisplayName();
+        return ("x:" . $outX . ", y:" . $outY . ", z:" . $outZ . ". On: " . $worldName);
     }
-  }
 
-  public function onHurt(EntityDamageEvent $event) {
-    if($event instanceof EntityDamageByEntityEvent && $event->getDamager() instanceof Player) {
-      $p = $event->getDamager();
-      if($this->isBanned($p->getInventory()->getItemInHand())) {
-        if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.bypass"))) {
-          $p->sendMessage("[BanItem] That item is banned.");
-          $event->setCancelled();
+    public function onTap(PlayerItemUseEvent $event): void {
+        $p = $event->getPlayer();
+        if($this->isBanned($event->getItem())) {
+            $this->getLogger()->info($p->getName() . " tried a Banned Item: " . $event->getItem() . " at " . $this->getPlayerPosition($p));
+            foreach($this->getServer()->getOnlinePlayers() as $player) {
+                if(isset($this->spys[strtolower($player->getName())])) {
+                    $player->sendMessage($p->getName() . " tried a Banned Item: " . $event->getItem());
+                }
+            }
+            if(!$p->hasPermission("banitem.*") || $p->hasPermission("banitem.bypass")) {
+                $p->sendMessage("[BanItem] That item is banned.");
+                $event->cancel();
+            }
         }
-      }
     }
-  }
 
-  public function onEat(PlayerItemConsumeEvent $event) {
-    $p = $event->getPlayer();
-    if($this->isBanned($event->getItem())) {
-      if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.bypass"))) {
-        $p->sendMessage("[BanItem] That item is banned.");
-        $event->setCancelled();
-      }
-    }
-  }
-
-  public function onShoot(EntityShootBowEvent $event) {
-    if($event->getEntity() instanceof Player) {
-      $p = $event->getEntity();
-      if($this->isBanned($event->getBow())) {
-        if(!($p->hasPermission("banitem") || $p->hasPermission("banitem.bypass"))) {
-          $p->sendMessage("[BanItem] That item is banned.");
-          $event->setCancelled();
+    public function onTouch(PlayerInteractEvent $event): void {
+        $p = $event->getPlayer();
+        if($this->isBanned($event->getItem())) {
+            $this->getLogger()->info($p->getName() . " tried a Banned Item: " . $event->getItem() . " at " . $this->getPlayerPosition($p));
+            foreach($this->getServer()->getOnlinePlayers() as $player) {
+                if(isset($this->spys[strtolower($player->getName())])) {
+                    $player->sendMessage($p->getName() . " tried a Banned Item: " . $event->getItem());
+                }
+            }
+            if(!$p->hasPermission("banitem.*") || !$p->hasPermission("banitem.bypass")) {
+                $p->sendMessage("[BanItem] That item is banned.");
+                $event->cancel();
+            }
         }
-      }
     }
-  }
 
-  public function onCommand(CommandSender $p,Command $cmd,$label,array $args) {
-    if(!isset($args[0])) {
-      return false;
+    public function onBlockPlace(BlockPlaceEvent $event): void {
+        $p = $event->getPlayer();
+        if($this->isBanned($event->getItem())) {
+            $this->getLogger()->info($p->getName() . " tried a Banned Item: " . $event->getItem() . " at " . $this->getPlayerPosition($p));
+            foreach($this->getServer()->getOnlinePlayers() as $player) {
+                if(isset($this->spys[strtolower($player->getName())])) {
+                    $player->sendMessage($p->getName() . " tried a Banned Item: " . $event->getItem());
+                }
+            }
+            if($p->hasPermission("banitem.*") || $p->hasPermission("banitem.bypass")) {
+                $event->cancel();
+            }
+        }
     }
-    if(strtolower($args[0]) == "ban" || strtolower($args[0]) == "unban") {
-      if(!isset($args[1])) {
-        return false;
-      }
-      $item = explode(":",$args[1]);
-      if(!is_numeric($item[0]) || (isset($item[1]) && !is_numeric($item[1]))) {
-        $p->sendMessage("[BanItem] §cPlease only use an item's ID value, and damage if needed.");
+
+    public function onHurt(EntityDamageByEntityEvent $event): void {
+        $p = $event->getDamager();
+        if($p instanceof Player) {
+            if($this->isBanned($p->getInventory()->getItemInHand())) {
+                foreach($this->getServer()->getOnlinePlayers() as $player) {
+                    if(isset($this->spys[strtolower($player->getName())])) {
+                        $player->sendMessage($p->getName() . " tried a Banned Item: " . $p->getInventory()->getItemInHand());
+                    }
+                }
+                if(!$p->hasPermission("banitem.*") || !$p->hasPermission("banitem.bypass")) {
+                    $p->sendMessage("[BanItem] That item is banned.");
+                    $event->cancel();
+                }
+            }
+        }
+    }
+
+    public function onEat(PlayerItemConsumeEvent $event): void {
+        $p = $event->getPlayer();
+        if($this->isBanned($event->getItem())) {
+            $this->getLogger()->info($p->getName() . " tried a Banned Item: " . $event->getItem() . " at " . $this->getPlayerPosition($p));
+            foreach($this->getServer()->getOnlinePlayers() as $player) {
+                if(isset($this->spys[strtolower($player->getName())])) {
+                    $player->sendMessage($p->getName() . " tried a Banned Item: " . $event->getItem());
+                }
+            }
+            if(!$p->hasPermission("banitem.*") || !$p->hasPermission("banitem.bypass")) {
+                $p->sendMessage("[BanItem] That item is banned.");
+                $event->cancel();
+            }
+        }
+    }
+
+    public function onShoot(EntityShootBowEvent $event): void {
+        $p = $event->getEntity();
+        if($p instanceof Player) {
+            if($this->isBanned($event->getBow())) {
+                foreach($this->getServer()->getOnlinePlayers() as $player) {
+                    if(isset($this->spys[strtolower($player->getName())])) {
+                        $player->sendMessage($p->getName() . " tried a Banned Item: " . $event->getBow());
+                    }
+                }
+                $this->getLogger()->info($p->getName() . " tried a Banned Item: " . $event->getBow() . " at " . $this->getPlayerPosition($p));
+                if(!$p->hasPermission("banitem.*") || !$p->hasPermission("banitem.bypass")) {
+                    $p->sendMessage("[BanItem] That item is banned.");
+                    $event->cancel();
+                }
+            }
+        }
+    }
+
+    public function onCommand(CommandSender $p, Command $cmd, string $label, array $args) : bool {
+        if(!isset($args[0])) {
+            return false;
+        }
+
+        if($args[0] === "report" && $p instanceof Player) {
+            if(isset($this->spys[strtolower($p->getName())])) {
+                unset($this->spys[strtolower($p->getName())]);
+            } else {
+                $this->spys[strtolower($p->getName())] = strtolower($p->getName());
+            }
+            if(isset($this->spys[strtolower($p->getName())])) {
+                $p->sendMessage(TEXTFORMAT::GREEN . "You have turned on banitem reports");
+            } else {
+                $p->sendMessage(TEXTFORMAT::GREEN . "You have turned off banitem reports");
+            }
+            return true;
+        }
+
+        if(!isset($args[1])) {
+            return false;
+        }
+
+        $item = StringToItemParser::getInstance()->parse($args[1]) ?? LegacyStringToItemParser::getInstance()->parse($args[1]);
+        if($item === null) {
+            $p->sendMessage("[BanItem] Please only use an item's ID value, and damage if needed.");
+            return true;
+        }
+
+        if($args[0] === "ban") {
+            $i = $item->getId();
+            if($item->getMeta() !== 0) {
+                $i = $i . "#" . $item->getMeta();
+            }
+            if(in_array($i, $this->items)) {
+                $p->sendMessage("[BanItem] That item is already banned.");
+            } else {
+                $this->items[] = $i;
+                $this->saveItems();
+                $p->sendMessage("[BanItem] " . str_replace("#", ":", $i) . " has been banned");
+                $this->getLogger()->info("[BanItem] " . str_replace("#", ":", $i) . " has been banned by " . $p->getName());
+            }
+        }elseif($args[0] === "unban") {
+            $i = $item->getId();
+            if($item->getMeta() !== 0) {
+                $i = $i . "#" . $item->getMeta();
+            }
+            if(!in_array($i, $this->items)) {
+                $p->sendMessage("[BanItem] That item wasn't banned.");
+            } else {
+                array_splice($this->items, array_search($i, $this->items), 1);
+                $this->saveItems();
+                $p->sendMessage("[BanItem] " . str_replace("#", ":", $i) . " has been unbanned.");
+                $this->getLogger()->info($p->getName() . " UnBanned Item: " . str_replace("#", ":", $i));
+            }
+        }
         return true;
-      }
     }
-    if(strtolower($args[0]) == "ban") {
-      $i = $item[0];
-      if(isset($item[1])) {
-        $i = $i . "#" . $item[1];
-      }
-      if(in_array($i,$this->items)) {
-        $p->sendMessage("[BanItem] §cThat item is already banned.");
-      } else {
-        array_push($this->items,$i);
-        $this->saveItems();
-        $p->sendMessage("[BanItem] §aThe item " . str_replace("#",":",$i) . " has been banned.");
-      }
-    } else if(strtolower($args[0]) == "unban") {
-      $i = $item[0];
-      if(isset($item[1])) {
-        $i = $i . "#" . $item[1];
-      }
-      if(!in_array($i,$this->items)) {
-        $p->sendMessage("[BanItem] §cThat item wasn't banned.");
-      } else {
-        array_splice($this->items,array_search($i,$this->items),1);
-        $this->saveItems();
-        $p->sendMessage("[BanItem] §aThe item " . str_replace("#",":",$i) . " has been unbanned.");
-      }
-    } else if(strtolower($args[0]) == "list") {
-      $p->sendMessage("[BanItem] §eBanned item" . (count($this->items) == 1 ? "" : "s") . ": §f" . str_replace("#", ":", implode(", ", $this->items)) . (count($this->items) > 0 ? "." : "§7None."));
-    } else {
-      return false;
-    }
-    return true;
-  }
 
-  public function isBanned($i) {
-    if(in_array(strval($i->getID()),$this->items,true) || in_array(($i->getID() . "#" . $i->getDamage()),$this->items,true)) {
-      return true;
+    public function isBanned(Item $i): bool {
+        if (in_array(strval($i->getId()), $this->items, true) || in_array(($i->getId() . "#" . $i->getMeta()), $this->items, true)) {
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-  public function saveItems() {
-    if(!isset($this->items)) {
-      if(!file_exists($this->getDataFolder() . "items.bin")) {
-        @mkdir($this->getDataFolder());
-        file_put_contents($this->getDataFolder() . "items.bin",json_encode(array()));
-      }
-      $this->items = json_decode(file_get_contents($this->getDataFolder() . "items.bin"),true);
+    public function saveItems(): void {
+        if(!isset($this->items)) {
+            if(!file_exists($this->getDataFolder() . "items.bin")) {
+                Filesystem::safeFilePutContents($this->getDataFolder() . "items.bin", json_encode([]));
+            }
+            $this->items = json_decode(file_get_contents($this->getDataFolder() . "items.bin"), true);
+        }
+        file_put_contents($this->getDataFolder() . "items.bin", json_encode($this->items));
     }
-    @mkdir($this->getDataFolder());
-    file_put_contents($this->getDataFolder() . "items.bin",json_encode($this->items));
-  }
-
 }
-?>
